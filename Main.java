f0946176d938097511fd9aaa170de98d5338fce7
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-12-04 19:23:34 +0000

    worked on

--- Main.java
+++ Main.java
@@ -10,17 +10,29 @@
        System.out.println("You are at the Mall! Your goal is to buy food at the food court...");
        // STEP #2: Get user command
        command = getCommand("There is multiple entrance for the mall. Would you like to go through door (1), (2), or (3)");
+       
       // STEP #3: Select a path based on the user's input
       if (command.equals("1")) {
          // New branch...
-         System.out.println("You reach the Carousel");
-      }  
-      else if (command.equals("n")){
-         System.out.println("You see a wedding at Tavern on the Greene");
+         System.out.println("You walk into Macy");
+         command = getCommand("You spent hours in Macy and apporching ULTA...How much money do you want to spend? (type a number)");
+         int money = Integer.parseInt(command);
+
+         if (money <= 40){
+            System.out.println("You didn't spend that much money, but all that walking up and down made you tired time to go home");
+         }
+         else {
+            System.out.println("You spent too much money...can't afford food court");
+         }
+      }
+      else if (command.equals("2")){
+         System.out.println("You enter Pink");
       }
       else{
          System.out.println("GAME OVER!");
       }
+        
+      
 
 
 
@@ -35,7 +47,6 @@
        System.out.println("\n▶︎▶︎▶︎ " + prompt);
        Scanner scan = new Scanner(System.in);
        String command = scan.nextLine().toLowerCase();
-       scan.close();
        return command;
     } // END OF METHOD
  } // END OF CLASS
